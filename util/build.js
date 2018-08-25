4158012532b6d003f0a5c91ecfce1f6f73635ace
idleberg <jan@idleberg.com>
2018-08-26 00:06:41 +0200

    add v3 pre-releases

--- util/build.js
+++ util/build.js
@@ -7,60 +7,61 @@ const { join } = require('path');
 const { versions } = require('./versions.json');
 const { writeFile } = require('fs');
 
-let hash = (blob) => {
-    const sha1 = hasha(blob, {algorithm: 'sha1'});
-    const sha256 = hasha(blob, {algorithm: 'sha256'});
-    const sha512 = hasha(blob, {algorithm: 'sha512'});
-    const md5 = hasha(blob, {algorithm: 'md5'});
+let getHash = (blob) => {
+  const sha1 = hasha(blob, {algorithm: 'sha1'});
+  const sha256 = hasha(blob, {algorithm: 'sha256'});
+  const sha512 = hasha(blob, {algorithm: 'sha512'});
+  const md5 = hasha(blob, {algorithm: 'md5'});
 
-    const hashes = [
-      sha256,
-      `md5:${md5}`,
-      `sha1:${sha1}`,
-      `sha512:${sha512}`
-    ];
+  const hashes = [
+    `md5:${md5}`,
+    `sha1:${sha1}`,
+    `sha256:${sha256}`,
+    `sha512:${sha512}`
+  ];
 
-    return hashes;
+  return hashes;
 };
 
 let template = (version, hashes) => {
-    const data = {
-        version: version,
-        majorVersion: version[0],
-        hashes: hashes
+  const data = {
+    version: version,
+    majorVersion: version[0],
+    hashes: hashes
+  }
+  ejs.renderFile(join(__dirname, '/manifest.ejs'), data, function(err, contents) {
+    if (err) {
+        console.error(symbol.error, err);
+        return;
     }
-    ejs.renderFile(join(__dirname, '/manifest.ejs'), data, function(err, contents) {
-        if (err) {
-            console.error(symbol.error, err);
-            return;
-        }
 
-        const outFile = `nsis-${version}.json`;
-        contents = JSON.stringify(JSON.parse(contents), null, 4);
+    const outFile = `nsis-${version}.json`;
+    contents = JSON.stringify(JSON.parse(contents), null, 4);
 
-        writeFile(outFile, contents, (err) => {
-          if (err) throw err;
-          console.log(symbol.success, `Saved nsis-${version}.json`);
-        });
-    })
+    writeFile(outFile, contents, (err) => {
+      if (err) throw err;
+      console.log(symbol.success, `Saved nsis-${version}.json`);
+    });
+})
 };
 
 versions.forEach( version => {
-    const major = version[0];
-    const url = `https://downloads.sourceforge.net/project/nsis/NSIS%20${major}/${version}/nsis-${version}.zip`;
+  const major = version[0];
+  const directory = (/\d(a|b|rc)\d*$/.test(version) === true) ? `NSIS%20${major}%20Pre-release` : `NSIS%20${major}`;
+  const url = `https://downloads.sourceforge.net/project/nsis/${directory}/${version}/nsis-${version}.zip`;
 
-    download(url)
-    .then(hash)
-    .then(hashes => {
-      return template(version, hashes);
-    })
-    .catch( error => {
-        if (error.statusMessage) {
-          if (error.statusMessage === 'Too Many Requests') {
-            return console.warn(symbol.warning, `${error.statusMessage}: nsis-${version}.zip`);
-          }
-          return console.error(symbol.error, `${error.statusMessage}: nsis-${version}.zip`);
-        }
-        console.error(symbol.error, error);
-    });
+  download(url)
+  .then(getHash)
+  .then(hashes => {
+    return template(version, hashes);
+  })
+  .catch( error => {
+    if (error.statusMessage) {
+      if (error.statusMessage === 'Too Many Requests') {
+        return console.warn(symbol.warning, `${error.statusMessage}: nsis-${version}.zip`);
+      }
+      return console.error(symbol.error, `${error.statusMessage}: nsis-${version}.zip`);
+    }
+    console.error(symbol.error, error);
+  });
 });
--- util/test.js
+++ util/test.js
@@ -6,35 +6,34 @@ const { join } = require('path');
 const { versions } = require('./versions.json');
 const { readFileSync } = require('fs');
 
-let returnHash = (blob) => {
-    return hasha(blob);
+let getHash = (blob) => {
+  return hasha(blob);
 };
 
 versions.forEach( version => {
-    const major = version[0];
-    const url = `https://downloads.sourceforge.net/project/nsis/NSIS%20${major}/${version}/nsis-${version}.zip`;
+  const major = version[0];
+  const url = `https://downloads.sourceforge.net/project/nsis/NSIS%20${major}/${version}/nsis-${version}.zip`;
 
-    test(`NSIS v${version}`, t => {
+  test(`NSIS v${version}`, t => {
+    return Promise.resolve(download(url)
+    .then(file => {
+      const manifest = readFileSync(join(__dirname, '..', `nsis-${version}.json`), 'utf8');
 
-      return Promise.resolve(download(url)
-      .then(file => {
-        const manifest = readFileSync(join(__dirname, '..', `nsis-${version}.json`), 'utf8');
+      const actual = hasha(file, {algorithm: 'sha256'});
+      const expected = JSON.parse(manifest).hash[0];
 
-        const actual = hasha(file, {algorithm: 'sha256'});
-        const expected = JSON.parse(manifest).hash[0];
-
-        t.is(actual, expected);
-      })
-      .catch( error => {
-          if (error.statusMessage) {
-            t.log(`Skipping Test: ${error.statusMessage}`);
-            t.pass();
-          } else if (error.code === 'ENOENT') {
-            t.log(`Skipping Test: Manifest Not Found`);
-            t.pass();
-          } else {
-            t.fail();
-          }
-      }));
-    });
+      t.is(actual, expected);
+    })
+    .catch( error => {
+      if (error.statusMessage) {
+        t.log(`Skipping Test: ${error.statusMessage}`);
+        t.pass();
+      } else if (error.code === 'ENOENT') {
+        t.log(`Skipping Test: Manifest Not Found`);
+        t.pass();
+      } else {
+        t.fail();
+      }
+    }));
+  });
 });
